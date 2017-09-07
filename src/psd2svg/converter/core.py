c09a7440ab19e3b95f2b4f83e5c9165c3481c656
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2017-09-07 14:40:58 +0900

    Split shape conversion

--- src/psd2svg/converter/core.py
+++ src/psd2svg/converter/core.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from logging import getLogger
 import psd_tools
-from psd2svg.converter.constants import BLEND_MODE, BLEND_MODE2
+from psd2svg.converter.constants import BLEND_MODE
 from psd2svg.utils.xml import safe_utf8
 
 
@@ -192,109 +192,6 @@ class LayerConverter(object):
         opacity = record.opacity / 255.0
         fill_opacity = dict(record.tagged_blocks).get(b'iOpa', 255) / 255.0
         return opacity, fill_opacity
-
-    def _get_vector_stroke(self, layer, target):
-        blocks = layer._tagged_blocks
-        if b'vstk' in blocks and (b'vsms' in blocks or b'vmsk' in blocks):
-            vstk = dict(blocks[b'vstk'].data.items)
-            # vstk defines bezier curves.
-            if vstk.get(b'strokeEnabled').value:
-                vsms = blocks.get(b'vsms', blocks.get(b'vmsk'))
-                return self._add_vstk(vstk, vsms, target.get_iri(),
-                                      layer.bbox)
-        return None
-
-    def _generate_path(self, vsms, command='C'):
-        # Iterator for SVG path constructor.
-        anchors = [p for p in vsms.path if p['selector'] in (1, 2)]
-
-        # Initial point.
-        yield 'M'
-        yield anchors[0]['anchor'][1] * self.width
-        yield anchors[0]['anchor'][0] * self.height
-        yield command
-
-        # Rest of the points.
-        for p1, p2 in zip(anchors, anchors[1:] + anchors[0:1]):
-            yield p1['control_leaving_knot'][1] * self.width
-            yield p1['control_leaving_knot'][0] * self.height
-            yield p2['control_preceding_knot'][1] * self.width
-            yield p2['control_preceding_knot'][0] * self.height
-            yield p2['anchor'][1] * self.width
-            yield p2['anchor'][0] * self.height
-
-        # Closing if needed.
-        closed = True
-        for p in vsms.path:
-            if p['selector'] == 3:
-                closed = False
-                break
-        if closed:
-            yield 'Z'
-
-    def _add_vstk(self, vstk, vsms, target_iri, bbox):
-        line_style = vstk[b'strokeStyleLineAlignment'].value
-        target = self._dwg.path(self._generate_path(vsms, 'C'), fill='none')
-
-        stroke_width = int(vstk[b'strokeStyleLineWidth'].value)
-        if line_style == b'strokeStyleAlignInside':
-            clippath = self._dwg.defs.add(self._dwg.clipPath())
-            clippath['class'] = 'stroke-inside'
-            clippath.add(self._dwg.path(self._generate_path(vsms)))
-            target['stroke-width'] = stroke_width * 2
-            target['clip-path'] = clippath.get_funciri()
-        elif line_style == b'strokeStyleAlignOutside':
-            mask = self._dwg.defs.add(self._dwg.mask())
-            mask['class'] = 'stroke-outside'
-            mask.add(self._dwg.rect(
-                insert=(bbox.x1, bbox.y1), size=(bbox.width, bbox.height),
-                fill='white'))
-            mask.add(self._dwg.path(self._generate_path(vsms), fill='black'))
-            target['stroke-width'] = stroke_width * 2
-            target['mask'] = mask.get_funciri()
-        else:
-            target['stroke-width'] = stroke_width
-
-        target['class'] = 'vector-stroke'
-
-        style = dict(vstk[b'strokeStyleContent'].items)
-        if vstk.get(b'strokeStyleContent').classID == b'solidColorLayer':
-            target['stroke'] = self._get_color_in_item(style)
-        elif vstk.get(b'strokeStyleContent').classID == b'patternLayer':
-            pattern = self._make_pattern(style, insert=(bbox.x1, bbox.y1))
-            target['stroke'] = pattern.get_funciri()
-        else:
-            grad = self._make_gradient(style, (bbox.width, bbox.height))
-            target['stroke'] = grad.get_funciri()
-
-        target['stroke-opacity'] = vstk[b'strokeStyleOpacity'].value / 100.0
-
-        cap_type = vstk[b'strokeStyleLineCapType'].value
-        if cap_type == b'strokeStyleButtCap':
-            target['stroke-linecap'] = 'butt'
-        elif cap_type == b'strokeStyleRoundCap':
-            target['stroke-linecap'] = 'round'
-        elif cap_type == b'strokeStyleSquareCap':
-            target['stroke-linecap'] = 'square'
-
-        join_type = vstk[b'strokeStyleLineJoinType'].value
-        if join_type == b'strokeStyleMiterJoin':
-            target['stroke-linejoin'] = 'miter'
-        elif join_type == b'strokeStyleRoundJoin':
-            target['stroke-linejoin'] = 'round'
-        elif join_type == b'strokeStyleBevelJoin':
-            target['stroke-linejoin'] = 'bevel'
-
-        offset = int(vstk[b'strokeStyleLineDashOffset'].value)
-        if offset:
-            target['stroke-dashoffset'] = offset
-
-        blend_mode = BLEND_MODE2.get(
-            vstk[b'strokeStyleBlendMode'].value, 'normal')
-        if blend_mode != 'normal':
-            target['style'] = 'mix-blend-mode: {}'.format(blend_mode)
-
-        return target
 
     def _add_photoshop_view(self):
         # Embed original photoshop view.
