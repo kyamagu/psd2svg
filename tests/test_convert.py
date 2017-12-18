f46b74189ce4ff5ea82d17c704f7e781dad3f40e
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2017-12-18 15:39:54 +0900

    Add stream and PSDImage IO

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,6 +1,8 @@
 import os
 import pytest
+import io
 from glob import glob
+from psd_tools import PSDImage
 from psd2svg import psd2svg
 
 FIXTURES = [
@@ -11,3 +13,27 @@ FIXTURES = [
 @pytest.mark.parametrize('psd_file', FIXTURES)
 def test_convert(tmpdir, psd_file):
     psd2svg(psd_file, tmpdir.dirname)
+
+
+@pytest.mark.parametrize('psd_file', FIXTURES[0:1])
+def test_input_io(tmpdir, psd_file):
+    with open(psd_file, "rb") as f:
+        assert isinstance(psd2svg(f), bytes)
+
+
+@pytest.mark.parametrize('psd_file', FIXTURES[0:1])
+def test_input_psd(tmpdir, psd_file):
+    psd = PSDImage.load(psd_file)
+    psd2svg(psd)
+
+
+@pytest.mark.parametrize('psd_file', FIXTURES[0:1])
+def test_input_layer(tmpdir, psd_file):
+    psd = PSDImage.load(psd_file)
+    assert psd2svg(psd.layers[2]).startswith(b"<")
+
+
+@pytest.mark.parametrize('psd_file', FIXTURES[0:1])
+def test_output_io(tmpdir, psd_file):
+    with io.BytesIO() as f:
+        assert f == psd2svg(psd_file, f)
