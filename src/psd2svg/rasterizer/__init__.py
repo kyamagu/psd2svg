6c7eaf371ccd8a23521cbcfd247f972f78c1e7e6
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2018-01-26 16:29:38 +0900

    Add quality test

---
+++ src/psd2svg/rasterizer/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from importlib import import_module
+
+
+def create_rasterizer(name="inkscape", *args, **kwargs):
+    module_name = "psd2svg.rasterizer.{}_rasterizer".format(name.lower())
+    class_name = "{}Rasterizer".format(name.capitalize())
+    cls = getattr(import_module(module_name), class_name)
+    assert cls is not None, "Invalid class name: {}".format(class_name)
+    return cls(*args, **kwargs)
