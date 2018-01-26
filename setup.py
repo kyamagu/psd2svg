5c47d8f57acaae6f537ea1a308885e2246587445
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2018-01-26 16:29:38 +0900

    Add quality test

--- setup.py
+++ setup.py
@@ -37,7 +37,12 @@ setup(
     author_email='KotaYamaguchi1984@gmail.com',
     license='MIT License',
     package_dir={'': 'src'},
-    packages=['psd2svg', 'psd2svg.converter', 'psd2svg.utils'],
+    packages=[
+        'psd2svg',
+        'psd2svg.converter',
+        'psd2svg.utils',
+        'psd2svg.rasterizer'
+    ],
     install_requires=[
         'pillow',
         'svgwrite',
---
+++ src/psd2svg/rasterizer/inkscape_rasterizer.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Inkscape rasterizer module.
+
+Prerequisite:
+
+    sudo apt-get install -y inkscape
+
+"""
+from __future__ import absolute_import, unicode_literals
+
+from PIL import Image
+import contextlib
+import tempfile
+import logging
+import os
+import shutil
+import sys
+import subprocess
+
+logger = logging.getLogger(__file__)
+
+
+@contextlib.contextmanager
+def temporary_directory(*args, **kwargs):
+    d = tempfile.mkdtemp(*args, **kwargs)
+    try:
+        yield d
+    finally:
+        shutil.rmtree(d)
+
+
+class InkscapeRasterizer(object):
+    """Inkscape rasterizer."""
+
+    def __init__(self, executable_path="inkscape", **kwargs):
+        self.executable_path = executable_path
+
+    def rasterize(self, url, size=None, format="png"):
+        with temporary_directory() as tempdir:
+            output_file = os.path.join(tempdir, "output.{}".format(format))
+            cmd = [os.path.abspath(url), "-e", output_file]
+            if size:
+                cmd += ["-w", size[0], "-h", size[1]]
+            proc = subprocess.check_call(
+                [self.executable_path, "-z"] + cmd,
+                stdout=sys.stdout, stderr=sys.stdout)
+            assert os.path.exists(output_file)
+            return Image.open(output_file)
