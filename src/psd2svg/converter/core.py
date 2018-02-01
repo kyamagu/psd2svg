299c7c1424aba29e22c8b6876fbffb76fa73db42
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2018-02-01 17:59:50 +0900

    Refactor layer converter mixin

--- src/psd2svg/converter/core.py
+++ src/psd2svg/converter/core.py
@@ -18,21 +18,27 @@ class LayerConverter(object):
         if layer.is_group():
             element = self.create_group(layer)
 
-        elif layer.has_pixels():
+        elif layer.has_relevant_pixels():
             element = self.create_image(layer)
             if layer.kind == 'type':
                 # TODO: Embed text metadata.
+                # text = self._get_text(layer)
                 pass
             if layer.kind == 'shape':
                 # TODO: Embed vector graphics.
                 pass
+
         else:
+            # Boxless element is either shape fill or adjustment.
             # element = self._get_adjustments(layer)
-            return None
+            element = self.create_shape(layer)
+
+        # TODO: vector stroke.
+        # self._get_vector_stroke(layer, target)
 
         element = self.add_attributes(layer, element)
         mask_element = self.create_mask(layer)
-        if mask_element:
+        if mask_element and not layer.mask.disabled:
             element['mask'] = mask_element.get_funciri()
 
         element = self.add_effects(layer, element)
@@ -50,44 +56,59 @@ class LayerConverter(object):
             if len(child_layer.clip_layers) > 0:
                 clip_group = self.create_clipping(child_layer, child_element)
                 element.add(clip_group)
+
+                # TODO: Blending option.
+                # layer.get_tag(TaggedBlock.BLEND_CLIPPING_ELEMENTS)
         return element
 
     def create_image(self, layer):
+        """Create a pixel object."""
         element = self._dwg.image(
             self._get_image_href(layer.as_PIL()),
-            insert=(layer.bbox.x1, layer.bbox.y1),
-            size=(layer.bbox.width, layer.bbox.height),
+            insert=(layer.left, layer.top),
+            size=(layer.width, layer.height),
             debug=False)  # To disable attribute validation.
         return element
 
+    def create_shape(self, layer):
+        """Create an adjustment element."""
+        if layer.has_box():
+            element = self._dwg.rect(
+                insert=(layer.left, layer.top),
+                size=(layer.width, layer.height),
+                fill="none")
+        else:
+            element = self._dwg.rect(
+                insert=(self._psd.bbox.x1, self._psd.bbox.y1),
+                size=(self._psd.bbox.width, self._psd.bbox.height),
+                fill="none")
+        # TODO: Create a backdrop-filter.
+        return element
+
     def create_mask(self, layer):
-        mask_data = layer.mask
-        if not mask_data or not mask_data.is_valid() or \
-                mask_data.mask_data.flags.mask_disabled:
+        """Create a mask."""
+        if not layer.has_mask() or not layer.mask.has_box():
             return None
-        background_color = mask_data.mask_data.real_background_color
-        if background_color is None:
-            background_color = mask_data.background_color
 
         # In SVG, mask needs a default rect.
-        default_bbox = layer.bbox
-        if not default_bbox:
-            default_bbox = BBox(0, 0, self.width, self.height)
+        viewbox = layer.bbox
+        if viewbox.is_empty():
+            viewbox = BBox(0, 0, self.width, self.height)
         mask_element = self._dwg.defs.add(self._dwg.mask(
-            size=(default_bbox.width, default_bbox.height)))
+            size=(viewbox.width, viewbox.height)))
         mask_element.add(self._dwg.rect(
-            insert=(default_bbox.x1, default_bbox.y1),
-            size=(default_bbox.width, default_bbox.height),
-            fill='rgb({0},{0},{0})'.format(background_color)))
-        bbox = mask_data.bbox
+            insert=(viewbox.x1, viewbox.y1),
+            size=(viewbox.width, viewbox.height),
+            fill='rgb({0},{0},{0})'.format(layer.mask.background_color)))
         mask_element.add(self._dwg.image(
-            self._get_image_href(mask_data.as_PIL()),
-            size=(bbox.width, bbox.height),
-            insert=(bbox.x1, bbox.y1)))
+            self._get_image_href(layer.mask.as_PIL()),
+            size=(layer.mask.width, layer.mask.height),
+            insert=(layer.mask.left, layer.mask.top)))
         mask_element['color-interpolation'] = 'sRGB'
         return mask_element
 
     def create_clipping(self, layer, clip_element):
+        """Create clipped elements."""
         # Create a mask for this clip element.
         mask = self._dwg.defs.add(self._dwg.mask())
         use = self._dwg.use(clip_element.get_iri())
@@ -103,7 +124,19 @@ class LayerConverter(object):
                 element.add(clipped_element)
         return element
 
+    def create_preview(self, hidden=True):
+        """Create a preview image of the photoshop."""
+        element = self._dwg.image(
+            self._get_image_href(self._psd.as_PIL()),
+            insert=(0, 0),
+            size=(self.width, self.height))
+        element['class'] = 'photoshop-image'
+        if hidden:
+            element['visibility'] = 'hidden'
+        return element
+
     def add_attributes(self, layer, element):
+        """Add layer attributes such as blending or visibility options."""
         element.set_desc(title=safe_utf8(layer.name))
         element['class'] = 'psd-layer psd-{}'.format(layer.kind)
         if not layer.visible:
@@ -116,6 +149,7 @@ class LayerConverter(object):
         return element
 
     def add_effects(self, layer, element):
+        """Add effects to the element."""
         effects = layer.effects
         fill_opacity = layer.get_tag(
             TaggedBlock.BLEND_FILL_OPACITY, 255) / 255.0
@@ -127,203 +161,5 @@ class LayerConverter(object):
         interior_blend_mode = None
         if layer.get_tag(TaggedBlock.BLEND_INTERIOR_ELEMENTS, False):
             interior_blend_mode = BLEND_MODE.get(layer.blend_mode, 'normal')
-        return self._add_effects(layer, element, effects, fill_opacity,
+        return self._add_effects(layer.effects, layer, element, fill_opacity,
                                  interior_blend_mode)
-
-
-    #
-    # Deprecate the following.
-    #
-
-
-    def _add_group(self, layers):
-        for layer in reversed(layers):
-            self._add_layer(layer)
-            for clip in reversed(layer.clip_layers):
-                self._add_layer(clip)
-
-    def _add_layer(self, layer):
-        target = self._get_target(layer)
-        if target is None:
-            return
-
-        target.get_iri()  # Assign id
-        mask = self._add_mask_if_exist(layer)
-        effects = self._get_effects(layer)
-        vector_stroke = self._get_vector_stroke(layer, target)
-
-        logger.debug('{} {} {}'.format(layer.name, target, mask))
-
-        if mask:
-            target['mask'] = mask.get_funciri()
-
-        # Blending options.
-        if not layer.visible:
-            target['visibility'] = 'hidden'
-            if vector_stroke:
-                vector_stroke['visibility'] = 'hidden'
-        blend_mode = BLEND_MODE.get(layer.blend_mode, 'normal')
-        if not blend_mode == 'normal':
-            target['style'] = 'mix-blend-mode: {}'.format(blend_mode)
-        opacity, fill_opacity = self._get_opacity(layer)
-        if opacity < 1.0:
-            target['opacity'] = opacity
-
-        if not effects and fill_opacity < 1.0:
-            target['opacity'] = opacity * fill_opacity
-
-        # TODO: Filter to strokes requires a different approach.
-        blocks = layer.tagged_blocks
-
-        if effects:
-            interior_blend_mode = None
-            if blocks.get(TaggedBlock.BLEND_INTERIOR_ELEMENTS, False):
-                interior_blend_mode = blend_mode
-            target = self._add_effects(effects, layer, target, fill_opacity,
-                                       interior_blend_mode)
-
-        if (layer != self._input_layer and
-                layer._record.clipping and self._clip_group):
-            self._clip_group.add(target)
-
-            # Acutally clipping with mask and mix-blend-mode does not
-            # correctly blend in SVG. clipPath does, but then cannot use
-            # bitmap for clipping. Any workaround?
-            self._clip_group['style'] = 'mix-blend-mode: {}'.format(
-                blend_mode)
-            if not self._clbl:
-                self._clip_group.attribs['style'] += '; isolation: isolate;'
-
-        elif layer != self._input_layer and layer._record.clipping:
-            # Convert the last target to a clipping mask
-            last_stroke = None
-            last_target = self._current_group.elements[-1]
-            if 'vector-stroke' in last_target.attribs.get(
-                    'class', '').split():
-                last_stroke = self._current_group.elements.pop()
-                last_target = self._current_group.elements[-1]
-            mask = self._dwg.defs.add(self._dwg.mask())
-            mask_bbox = layer.bbox
-
-            use = self._dwg.use(last_target.get_iri())
-            use['filter'] = self._get_white_filter().get_funciri()
-            mask.add(use)
-
-            mask['color-interpolation'] = 'sRGB'
-            self._clip_group = self._dwg.g(mask=mask.get_funciri())
-            self._clip_group['class'] = 'clipping-mask'
-            self._current_group.add(self._clip_group)
-            self._clip_group.add(target)
-
-            # Acutally clipping with mask and mix-blend-mode does not
-            # correctly blend in SVG. clipPath does, but then cannot use
-            # bitmap for clipping. Any workaround?
-            self._clip_group['style'] = 'mix-blend-mode: {}'.format(
-                blend_mode)
-            if not self._clbl:
-                self._clip_group.attribs['style'] += '; isolation: isolate;'
-
-            if last_stroke:  # Move the stroke to the front.
-                self._current_group.add(last_stroke)
-        else:
-            self._clip_group = None
-            self._current_group.add(target)
-
-            # Keep info for when next layer is clip group
-            self._clbl = blocks.get(TaggedBlock.BLEND_CLIPPING_ELEMENTS)
-
-        if vector_stroke:
-            self._current_group.add(vector_stroke)
-
-    def _get_target(self, layer):
-        target = None
-        if layer.is_group():
-            # Group.
-            current_group = self._current_group
-            target = self._dwg.g()
-            target.set_desc(title=safe_utf8(layer.name))
-            target['class'] = 'manual-group'
-            self._current_group = target
-            self._add_group(layer.layers)
-            self._current_group = current_group
-        elif layer.has_pixels():
-            # Regular pixel layer.
-            target = self._dwg.image(
-                self._get_image_href(layer.as_PIL()),
-                insert=(layer.bbox.x1, layer.bbox.y1),
-                size=(layer.bbox.width, layer.bbox.height),
-                debug=False)  # To disable attribute validation.
-            target.set_desc(title=safe_utf8(layer.name))
-
-            text = self._get_text(layer)
-            if text:
-                text['class'] = 'text-text'
-                if self.text_mode == 'text-only':
-                    text.set_desc(title=safe_utf8(layer.name))
-                    target = text
-                elif self.text_mode == 'image-only':
-                    pass
-                else:
-                    target['class'] = 'text-image'
-                    container = self._dwg.g()
-                    container.elements.append(target.elements.pop(0))
-                    container.add(target)
-                    container.add(text)
-                    container['class'] = 'text-container'
-                    target = container
-        elif layer.kind == 'shape':
-            anchors = layer.get_anchors()
-            if not anchors:
-                return None
-            fill = self._get_fill(layer)
-            target = self._dwg.polygon(points=anchors, fill=fill)
-            target.set_desc(title=safe_utf8(layer.name))
-        else:
-            target = self._get_adjustments(layer)
-            if target:
-                logger.debug('{} {}'.format(layer.name, target))
-            else:
-                logger.warning('Not rendered: {}'.format(layer))
-            return None
-        return target
-
-    def _add_mask_if_exist(self, layer):
-        mask_data = layer.mask
-        if not mask_data or not mask_data.is_valid() or \
-                mask_data.mask_data.flags.mask_disabled:
-            return None
-        background_color = mask_data.mask_data.real_background_color
-        if background_color is None:
-            background_color = mask_data.background_color
-
-        # In SVG, mask needs a default rect.
-        default_bbox = layer.bbox
-        if not default_bbox:
-            default_bbox = BBox(0, 0, self.width, self.height)
-        mask = self._dwg.defs.add(self._dwg.mask(
-            size=(default_bbox.width, default_bbox.height)))
-        mask.add(self._dwg.rect(
-            insert=(default_bbox.x1, default_bbox.y1),
-            size=(default_bbox.width, default_bbox.height),
-            fill='rgb({0},{0},{0})'.format(background_color)))
-        bbox = mask_data.bbox
-        mask.add(self._dwg.image(
-            self._get_image_href(mask_data.as_PIL()),
-            size=(bbox.width, bbox.height),
-            insert=(bbox.x1, bbox.y1)))
-        mask['color-interpolation'] = 'sRGB'
-        return mask
-
-    def _get_opacity(self, layer):
-        opacity = layer.opacity / 255.0
-        fill_opacity = layer.get_tag(
-            TaggedBlock.BLEND_FILL_OPACITY, 255) / 255.0
-        return opacity, fill_opacity
-
-    def _add_photoshop_view(self):
-        # Embed original photoshop view.
-        image = self._psd.as_PIL()
-        original = self._dwg.add(self._dwg.image(
-            self._get_image_href(image), insert=(0, 0),
-            size=(self.width, self.height), visibility='hidden'))
-        original['class'] = 'photoshop-image'
