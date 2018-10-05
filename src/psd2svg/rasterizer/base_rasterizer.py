1947cdf2be3a4967e8b674191b590ec571caaa52
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2018-10-05 12:07:54 +0900

    Add rasterize_from_string method

--- src/psd2svg/rasterizer/base_rasterizer.py
+++ src/psd2svg/rasterizer/base_rasterizer.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import logging
+import tempfile
 from PIL import Image
 
 logger = logging.getLogger(__name__)
@@ -9,6 +10,13 @@ logger = logging.getLogger(__name__)
 
 class BaseRasterizer(object):
     """Base class for rasterizer implementation."""
+
+    def rasterize_from_string(self, input, **kwargs):
+        with tempfile.NamedTemporaryFile(suffix='.svg') as f:
+            f.write(input if isinstance(input, bytes)
+                    else input.encode('utf-8'))
+            f.flush()
+            return self.rasterize(f.name, **kwargs)
 
     def rasterize(self, url, size=None, **kwargs):
         raise NotImplementedError
