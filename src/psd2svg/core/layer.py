93b98c242e740af90e50db588cc0ac8c466baf7a
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-09 18:31:18 +0900

    Add mask

--- src/psd2svg/core/layer.py
+++ src/psd2svg/core/layer.py
@@ -2,7 +2,6 @@ import logging
 from xml.etree import ElementTree as ET
 
 from psd_tools.api import layers
-from psd_tools.constants import BlendMode
 
 from psd2svg.core import svg_utils
 from psd2svg.core.base import ConverterProtocol
@@ -14,12 +13,20 @@ logger = logging.getLogger(__name__)
 class LayerConverter(ConverterProtocol):
     """Main layer converter mixin."""
 
+    _id_counter = 0
+
+    def auto_id(self, prefix: str = "") -> str:
+        """Generate a unique ID for SVG elements."""
+        self._id_counter += 1
+        return f"{prefix}{self._id_counter:g}"
+
     def add_layer(self, layer: layers.Layer) -> ET.Element | None:
         """Add a layer to the svg document."""
         if not layer.is_visible():
             # TODO: Option to include hidden layers.
-            logger.debug(f"Layer '{layer.name}' is not visible.")
+            logger.debug(f"Layer '{layer.name}' is invisible.")
             return None
+        logger.debug(f"Adding layer: '{layer.name}' ({layer.kind})")
 
         # Simple registry-based dispatch.
         registry = {
@@ -35,6 +42,7 @@ class LayerConverter(ConverterProtocol):
         node = handler(layer)
         if node is not None:
             self.add_attributes(layer, node)
+            self.add_mask(layer, node)
         return node
 
     def add_group(self, group: layers.Group) -> ET.Element | None:
@@ -74,3 +82,51 @@ class LayerConverter(ConverterProtocol):
         blend_mode = BLEND_MODE[layer.blend_mode]
         if blend_mode != "normal":
             svg_utils.add_style(node, "mix-blend-mode", blend_mode)
+
+    def add_mask(self, layer: layers.Layer, target: ET.Element) -> ET.Element | None:
+        """Add a mask to a layer node."""
+        if (
+            not layer.has_mask()
+            or layer.mask.disabled
+            or layer.mask.width == 0
+            or layer.mask.height == 0
+        ):
+            return None
+        logger.debug(f"Adding mask: '{layer.name}'")
+
+        # Viewbox for the mask. If the mask is empty, use the full canvas.
+        viewbox = layer.bbox
+        if viewbox == (0, 0, 0, 0):
+            viewbox = (0, 0, self.psd.width, self.psd.height)
+
+        # Create the mask node.
+        node = svg_utils.create_node(
+            "mask", parent=self.current, id=self.auto_id("mask_")
+        )
+        # TODO: Check layer mask attributes.
+        # node["color-interpolation"] = "sRGB"
+
+        # If the mask has a background color, add a white rectangle first.
+        if layer.mask.background_color > 0:
+            svg_utils.create_node(
+                "rect",
+                parent=node,
+                x=viewbox[0],
+                y=viewbox[1],
+                width=viewbox[2] - viewbox[0],
+                height=viewbox[3] - viewbox[1],
+                fill="white",
+            )
+        
+        # Mask image.
+        self.images.append(layer.mask.topil().convert("L"))
+        svg_utils.create_node(
+            "image",
+            parent=node,
+            x=layer.mask.left,
+            y=layer.mask.top,
+            width=layer.mask.width,
+            height=layer.mask.height,
+        )
+        target.set("mask", f"url(#{node.get('id')})")
+        return node
