34f9b5f5010fa4c38923540c2481df8fa29c0ebc
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-10 22:10:51 +0900

    Add basic tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,46 @@
+import logging
 import os
-from glob import glob
 
-FIXTURES = [
-    p for p in glob(
-        os.path.join(os.path.dirname(__file__), 'fixtures', '*.psd'))
-    ]
+logger = logging.getLogger(__name__)
+
+
+def get_fixture(name: str) -> str:
+    """Get a fixture by name."""
+    return os.path.join(os.path.dirname(__file__), "fixtures", name)
+
+
+ADJUSTMENTS = [
+    "layer-types/black-and-white.psd",
+    "layer-types/brightness-contrast.psd",
+    "layer-types/channel-mixer.psd",
+    "layer-types/color-balance.psd",
+    "layer-types/curves-with-vectormask.psd",
+    "layer-types/curves.psd",
+    "layer-types/exposure.psd",
+    "layer-types/gradient-map-v3-classic.psd",
+    "layer-types/gradient-map-v3-linear.psd",
+    "layer-types/gradient-map.psd",
+    "layer-types/hue-saturation.psd",
+    "layer-types/invert.psd",
+    "layer-types/levels.psd",
+    "layer-types/photo-filter.psd",
+    "layer-types/selective-color.psd",
+    "layer-types/threshold.psd",
+    "layer-types/vibrance.psd",
+]
+
+FILLS = [
+    "layer-types/gradient-fill.psd",
+    "layer-types/solid-color-fill.psd",
+    "layer-types/pattern-fill.psd",
+]
+
+TYPES = [
+    "layer-types/group.psd",
+    "layer-types/pixel-layer.psd",
+    "layer-types/shape-layer.psd",
+    "layer-types/smartobject-layer.psd",
+    "layer-types/type-layer.psd",
+]
+
+ALL = ADJUSTMENTS + FILLS + TYPES
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,10 +1,49 @@
 import pytest
+import os
 
+import imagehash
+import numpy as np
+import psd_tools
 from psd2svg import Converter
+from psd2svg.rasterizer.resvg_rasterizer import ResvgRasterizer
 
-from .conftest import FIXTURES
+from .conftest import ALL, TYPES, get_fixture
 
 
-@pytest.mark.parametrize('psd_file', FIXTURES)
+@pytest.mark.parametrize("psd_file", ALL)
 def test_convert(tmpdir, psd_file: str) -> None:
-    Converter.convert(psd_file, tmpdir.dirname + "/output.svg")
+    """Test converting PSD files to SVG."""
+
+    input_path = get_fixture(psd_file)
+    output_path = tmpdir.dirname + "/output.svg"
+    images_path = tmpdir.dirname + "/images"
+    Converter.convert(input_path, output_path, images_path)
+    assert os.path.exists(output_path)
+
+
+@pytest.mark.parametrize("psd_file", TYPES)
+def test_quality(tmpdir, psd_file: str) -> None:
+    """Test conversion quality in the raster format."""
+
+    input_path = get_fixture(psd_file)
+    svg_path = tmpdir.dirname + "/output.svg"
+    images_path = tmpdir.dirname + "/images"
+    rasterizer = ResvgRasterizer()
+
+    # Convert PSD to SVG.
+    Converter.convert(input_path, svg_path, images_path)
+    assert os.path.exists(svg_path)
+
+    # Rasterize SVG and compare with original PSD.
+    rasterized = rasterizer.rasterize(svg_path)
+    original = psd_tools.PSDImage.open(input_path).composite()
+
+    # Quality check.
+    assert rasterized.width == original.width
+    assert rasterized.height == original.height
+    input_hash = imagehash.phash(original)
+    output_hash = imagehash.phash(rasterized)
+    error_rate = np.sum(np.bitwise_xor(input_hash.hash, output_hash.hash)) / float(
+        input_hash.hash.size
+    )
+    assert error_rate <= 0.125
