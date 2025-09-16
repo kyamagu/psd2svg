5cace65a6afc918cfe12a66b32b9471c918787db
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-16 14:39:19 +0900

    Update quality test

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,13 +1,14 @@
-import pytest
+import logging
 import os
 
-import imagehash
 import numpy as np
 import psd_tools
+import pytest
+
 from psd2svg import Converter
 from psd2svg.rasterizer.resvg_rasterizer import ResvgRasterizer
 
-from .conftest import ALL, TYPES, get_fixture
+from .conftest import ALL, TYPES, FILLS, get_fixture
 
 
 @pytest.mark.parametrize("psd_file", ALL)
@@ -21,7 +22,7 @@ def test_convert(tmpdir, psd_file: str) -> None:
     assert os.path.exists(output_path)
 
 
-@pytest.mark.parametrize("psd_file", TYPES)
+@pytest.mark.parametrize("psd_file", TYPES + FILLS)
 def test_quality(tmpdir, psd_file: str) -> None:
     """Test conversion quality in the raster format."""
 
@@ -37,13 +38,16 @@ def test_quality(tmpdir, psd_file: str) -> None:
     # Rasterize SVG and compare with original PSD.
     rasterized = rasterizer.rasterize(svg_path)
     original = psd_tools.PSDImage.open(input_path).composite()
+    if original.mode != "RGBA":
+        original = original.convert("RGBA")
 
     # Quality check.
     assert rasterized.width == original.width
     assert rasterized.height == original.height
-    input_hash = imagehash.phash(original)
-    output_hash = imagehash.phash(rasterized)
-    error_rate = np.sum(np.bitwise_xor(input_hash.hash, output_hash.hash)) / float(
-        input_hash.hash.size
-    )
-    assert error_rate <= 0.125
+    assert rasterized.mode == original.mode
+
+    rasterized_array = np.array(rasterized, dtype=np.float32) / 255.0
+    original_array = np.array(original, dtype=np.float32) / 255.0
+    mae = np.nanmean(np.abs(rasterized_array - original_array))
+    logging.info(f"MAE for {psd_file}: {mae}")
+    assert mae < 0.05, f"MAE is too high: {mae}"
