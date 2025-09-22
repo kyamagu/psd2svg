2dcc07244746dec9c23a9782a6be7be52118a736
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-22 12:57:21 +0900

    Add helpers for num to str conversion

--- src/psd2svg/utils/xml.py
+++ src/psd2svg/utils/xml.py
@@ -10,4 +10,23 @@ ILLEGAL_XML_RE: Pattern[str] = re.compile(
 
 
 def safe_utf8(text: str) -> str:
+    """Remove illegal XML characters from text."""
     return ILLEGAL_XML_RE.sub(" ", text)
+
+
+def num2str(num: int | float | bool, format = "g") -> str:
+    """Convert a number to a string, using the specified format for floats."""
+    if isinstance(num, bool):
+        return "true" if num else "false"
+    if isinstance(num, int):
+        return str(num)
+    if isinstance(num, float):
+        if num.is_integer():
+            return str(int(num))
+        return format % num
+    raise ValueError(f"Unsupported type: {type(num)}")
+
+
+def seq2str(seq: list[int | float | bool], sep = ",", format = "g") -> str:
+    """Convert a sequence of numbers to a string, using the specified format for floats."""
+    return sep.join(num2str(n, format) for n in seq)
