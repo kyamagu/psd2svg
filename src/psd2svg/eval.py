136d74626ce802c977e048c9c4717f235c3901f4
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-29 09:10:05 +0900

    Add clipping quality tests

--- src/psd2svg/eval.py
+++ src/psd2svg/eval.py
@@ -4,12 +4,26 @@ import numpy as np
 import psd_tools
 from PIL.Image import Image
 
+from psd2svg.core.converter import Converter
 from psd2svg.rasterizer import create_rasterizer
 
 logger = logging.getLogger(__name__)
 
 
-def check_quality(psdimage: psd_tools.PSDImage, svg: str, metric: str) -> float:
+def compute_conversion_quality(psdimage: psd_tools.PSDImage, metric: str) -> float:
+    """Test conversion quality in the raster format."""
+
+    # Convert PSD to SVG.
+    converter = Converter(psdimage)
+    converter.build()
+    converter.embed_images()
+    svg = converter.export()
+
+    # Check quality.
+    return compute_raster_quality(psdimage, svg, metric)
+
+
+def compute_raster_quality(psdimage: psd_tools.PSDImage, svg: str, metric: str) -> float:
     """Test conversion quality in the raster format."""
 
     # Rasterize SVG and compare with original PSD.
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -5,7 +5,7 @@ import psd_tools
 import pytest
 
 from psd2svg import Converter
-from psd2svg.eval import check_quality
+from psd2svg.eval import compute_conversion_quality
 
 from .conftest import ALL, TYPES, FILLS, get_fixture
 
@@ -24,17 +24,34 @@ def test_convert(tmpdir, psd_file: str) -> None:
 @pytest.mark.parametrize("psd_file", TYPES + FILLS)
 def test_quality(psd_file: str) -> None:
     """Test conversion quality in the raster format."""
-
-    input_path = get_fixture(psd_file)
-
-    # Convert PSD to SVG.
-    psdimage = psd_tools.PSDImage.open(input_path)
-    converter = Converter(psdimage)
-    converter.build()
-    converter.embed_images()
-    svg = converter.export()
-
-    # Check quality.
-    score = check_quality(psdimage, svg, "MSE")
+    psdimage = psd_tools.PSDImage.open(get_fixture(psd_file))
+    score = compute_conversion_quality(psdimage, "MSE")
     logging.info(f"MSE for {psd_file}: {score}")
     assert score < 0.05, f"MSE is too high: {score}"
+
+
+@pytest.mark.parametrize(
+    "psd_file, quality",
+    [
+        pytest.param(
+            "clipping/group-with-clip-stroke-effect.psd",
+            0.02,
+            marks=pytest.mark.xfail(reason="Stroke effect is inaccurate."),
+        ),
+        pytest.param("clipping/group-with-clip-stroke.psd", 0.02),
+        pytest.param(
+            "clipping/pixel-with-clip-stroke-effect.psd",
+            0.02,
+            marks=pytest.mark.xfail(reason="Stroke effect is inaccurate."),
+        ),
+        pytest.param("clipping/shape-with-clip-stroke-effect.psd", 0.02),
+        pytest.param("clipping/shape-with-clip-stroke.psd", 0.02),
+        pytest.param("clipping/shape-with-clip2-stroke.psd", 0.02),
+    ],
+)
+def test_clipping(psd_file: str, quality: float) -> None:
+    """Test converting PSD files with clipping masks to SVG."""
+    psdimage = psd_tools.PSDImage.open(get_fixture(psd_file))
+    score = compute_conversion_quality(psdimage, "MSE")
+    logging.info(f"MSE for {psd_file}: {score} vs. {quality}")
+    assert score < quality, f"MSE is too high: {score} vs. {quality}"
